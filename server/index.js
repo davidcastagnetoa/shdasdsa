65a9851d8345b988b606987c6ab494ae5c7f4154
David Castagneto A <76570078+davidcastagnetoa@users.noreply.github.com>
2023-01-19 23:15:41 +0000

    server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
 import { register } from "./controllers/auth.js"
+import { verifyToken } from "./middleware/auth.js";
 
 // Configuration
 const __filename = fileURLToPath(import.meta.url);
@@ -39,7 +42,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Routes with files
-app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), verifyToken, register);
+
+// Routes
+ap.use("/auth/routes", authRoutes);
+app.use("/user", userRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/user.js";
+import { verifyToken  } from "../middleware/auth.js";
+
+
+const router = express.Router();
+
