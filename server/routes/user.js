4ba65de7e5e1f05a278af4f30ee7c6ddb100d865
David Castagneto A <76570078+davidcastagnetoa@users.noreply.github.com>
2023-01-20 00:18:22 +0000

    server

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,3 +9,12 @@ import { verifyToken  } from "../middleware/auth.js";
 
 const router = express.Router();
 
+// Read
+
+router.get( "/:id", verifyToken, getUser );
+router.get( "/:id/friends", verifyToken, getUserFriends );
+
+// Update
+router.patch( "/:id/:friendId", verifyToken, addRemoveFriend );
+
+export default router;
